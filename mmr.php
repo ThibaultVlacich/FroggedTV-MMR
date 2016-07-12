dbf7018a3d48eee827e3bb732d38a8ac729163e9
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-07-13 01:22:06 +0200

    Linting up mmr.php

--- mmr.php
+++ mmr.php
@@ -8,22 +8,33 @@
 
   $yaspProfileID = intval($_GET['yID']);
 
-  $arrContextOptions=array(
-    "ssl"=>array(
-      "verify_peer"=>false,
-      "verify_peer_name"=>false,
+  // Avoid a bug with SSL validation
+  $arrContextOptions = array(
+    "ssl" => array(
+      "verify_peer" => false,
+      "verify_peer_name" => false,
     ),
   );
 
+  // Get the Yasp profile of the player
   $file = file_get_contents('https://yasp.co/players/'.$yaspProfileID, false, stream_context_create($arrContextOptions));
 
+  // Search for the MMR on the page
   if (preg_match('/<span class="text-info rating"><span><abbr title="Solo MMR"><i class="fa fa-fw fa-user"><\/i><\/abbr><\/span><small style="margin-left:.5em">([0-9]{4})<\/small><\/span>/i', $file, $matches)) {
-    header('Content-Type: application/json');
+    // MMR founded
+    // Return a JSON containing the MMR founded
     echo json_encode(array('mmr' => $matches[1]));
+    
+    header('Content-Type: application/json');
+    
     die;
   } else {
+    // MMR not founded
+    // Returns an error
     echo 'Erreur: Impossible de trouver le MMR';
+    
     header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+    
     die;
   }
 ?>
